24554b61efb33fa7e197fed044e3750a94319ee4
Chery <376067609@qq.com>
2019-02-28 20:22:02 +0800

    页面框架

--- FineUIMvc.EmptyProject/Controllers/TreeController.cs
+++ FineUIMvc.EmptyProject/Controllers/TreeController.cs
@@ -8,10 +8,76 @@ namespace FineUIMvc.EmptyProject.Controllers
 {
     public class TreeController : Controller
     {
-        // GET: Tree
+        // GET: Tree 页面框架初始化
         public ActionResult Index()
         {
             return View();
         }
+
+        //ECharts示例1
+        public ActionResult ECharts_1()
+        {
+            return View();
+        }
+
+        public ActionResult echarts1_IFrame()
+        {
+            var barOptions = String.Empty;
+            var paramType = Request.QueryString["type"];         
+            
+            if (String.IsNullOrEmpty(paramType) || paramType == "sales")
+            {
+                //{
+                //    "title": {
+                //        "text": "销量图表"
+                //    },
+                //    "xAxis": {
+                //        "type": "category",
+                //        "data": ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+                //    },
+                //    "yAxis": {
+                //        "type": "value"
+                //    },
+                //    "series": [{
+                //        "name": "销量",
+                //        "type": "bar",
+                //        "data": [5.0, 20.0, 36.0, 10.0, 10.0, 20.0]
+                //    }]
+                //}
+                barOptions = "{\"color\":[\"#5793f3\"],\"title\":{\"text\":\"销量图表\"},\"xAxis\":{\"type\":\"category\",\"data\":[\"衬衫\",\"羊毛衫\",\"雪纺衫\",\"裤子\",\"高跟鞋\",\"袜子\"]},\"yAxis\":{\"type\":\"value\"},\"series\":[{\"name\":\"销量\",\"type\":\"bar\",\"data\":[5.0,20.0,36.0,10.0,10.0,20.0]}]}";
+            }
+            else
+            {
+                //{
+                //    "title": {
+                //        "text": "产量图表"
+                //    },
+                //    "xAxis": {
+                //        "type": "category",
+                //        "data": ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+                //    },
+                //    "yAxis": {
+                //        "type": "value"
+                //    },
+                //    "series": [{
+                //        "name": "销量",
+                //        "type": "bar",
+                //        "data": [6.0, 18.0, 17.0, 30.0, 20.0, 5.0]
+                //    }]
+                //}
+                barOptions = "{\"color\":[\"#d14a61\"],\"title\":{\"text\":\"产量图表\"},\"xAxis\":{\"type\":\"category\",\"data\":[\"衬衫\",\"羊毛衫\",\"雪纺衫\",\"裤子\",\"高跟鞋\",\"袜子\"]},\"yAxis\":{\"type\":\"value\"},\"series\":[{\"name\":\"销量\",\"type\":\"bar\",\"data\":[6.0,18.0,17.0,35.0,20.0,5.0]}]}";
+            }
+
+            ViewBag.BarOptions = barOptions;
+
+            return View();
+        }
+
+        public ActionResult ECharts_2()
+        {
+            return View();
+        }
+
+
     }
 }
