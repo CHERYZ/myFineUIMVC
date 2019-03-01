7ab121726d57cc5d93d46458e7265aac1a5069ba
Chery <376067609@qq.com>
2019-03-01 17:20:53 +0800

    示例页面2及示例3框架
    示例2：将ECharts代码写到前台页面
    示例3：完成与数据库交互

--- FineUIMvc.EmptyProject/Controllers/TreeController.cs
+++ FineUIMvc.EmptyProject/Controllers/TreeController.cs
@@ -18,9 +18,9 @@ namespace FineUIMvc.EmptyProject.Controllers
         public ActionResult ECharts_1()
         {
             return View();
-        }
+        } //ECharts表单
 
-        public ActionResult echarts1_IFrame()
+        public ActionResult echarts1_IFrame()  //ECharts页面
         {
             var barOptions = String.Empty;
             var paramType = Request.QueryString["type"];         
@@ -75,9 +75,93 @@ namespace FineUIMvc.EmptyProject.Controllers
 
         public ActionResult ECharts_2()
         {
+
             return View();
         }
+        public ActionResult ECharts2_IFrame()
+        {
+            var barOptions = String.Empty;
 
+            //{
+            //    "title": {
+            //        "text": "销量图表"
+            //    },
+            //    "xAxis": {
+            //        "type": "category",
+            //        "data": ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+            //    },
+            //    "yAxis": {
+            //        "type": "value"
+            //    },
+            //    "series": [{
+            //        "name": "销量",
+            //        "type": "bar",
+            //        "data": [5.0, 20.0, 36.0, 10.0, 10.0, 20.0]
+            //    }]
+            //}
+            barOptions = "{" +
+                "\"color\":[" +
+                "\"#5793f3\"" +
+                "]," +
+                "\"title\":{" +
+                "\"text\":\"销量图表\"" +
+                "}," +
+                "\"xAxis\":{" +
+                "\"type\":\"category\"," +
+                "\"data\":[\"衬衫\",\"羊毛衫\",\"雪纺衫\",\"裤子\",\"高跟鞋\",\"袜子\"]" +
+                "}," +
+                "\"yAxis\":{" +
+                "\"type\":\"value\"" +
+                "}," +
+                "\"series\":[{" +
+                "\"name\":\"销量\",\"type\":\"bar\",\"data\":[5.0,20.0,36.0,10.0,10.0,20.0]" +
+                "}]" +
+                "}";
+
+            /*
+                 xAxis: {
+                    type: 'category',
+                    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                series: [{
+                    data: [820, 932, 901, 934, 1290, 1330, 1320],
+                    type: 'line',
+                    smooth: true
+                }]
+             */
+            //"\"title\":{" +
+            //"\"text\":\"销量图表\"" +
+            //"}," +
+            //"\"xAxis\":{" +
+            //"\"type\":\"category\"," +
+            //"\"data\":[\"衬衫\",\"羊毛衫\",\"雪纺衫\",\"裤子\",\"高跟鞋\",\"袜子\"]" +
+            //"}," +
+
+            //barOptions = "{" +
+            //    "\"xAxis\":{"+
+            //    "\"type\":\"category","+
+            //    "\"data\":[\"Mon\", \"Tue\", \"Wed\", \"Thu\", \"Fri\", \"Sat\", \"Sun\"]"+
+            //    "}",+
+
+            //    "}";
+                
+
+            ViewBag.BarOptions = barOptions;
+
+            return View();
+        } //将ECharts代码写到前端视图页面
+
+        public ActionResult ECharts_3() //实例3：数据库交互
+        {
+            return View();
+        }
+        public ActionResult ECharts3_IFrame()
+        {
+            return View();
+        }
 
     }
 }
